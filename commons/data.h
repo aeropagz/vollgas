7ef044da72ff57e03e723ac074d732cae217196a
Nico Petersen <nico.b.petersen@student.fh-kiel.de>
2022-12-07 10:53:22 +0100

    Added Data-Struct

--- commons/data.h
+++ commons/data.h
@@ -1,7 +1,6 @@
-
 struct _data {
-    int motor1Speed;
-    int motor1Direction;
-    int motor2Speed;
-    int motor2Direction;
+    unsigned char motor1Speed;
+    unsigned char motor1Direction;
+    unsigned char motor2Speed;
+    unsigned char motor2Direction;
 } Data;
